ca5588933fb17218f7462d38a1e528eb62af3545
mlucz <major_mike18@hotmail.com>
2015-03-16 22:27:40 -0600

    PHP Fix

--- CreateDB.php
+++ CreateDB.php
@@ -3,15 +3,16 @@
 	<head>
 	</head>
 	<body>
-		<?php>
+		<?php
 			$server = "tcp:ts50nv9z1x.database.windows.net,1433";
             $user = "mjl566";
             $pwd = "Cmpt350!";
             $db = "cmpt350a2b";
+			
             try{
                 $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
                 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-                //echo "Connection successfully</br>";
+                echo "Connection successfully</br>";
             }
             catch(Exception $e){
                 die("Connection failed: ".print_r($e));
